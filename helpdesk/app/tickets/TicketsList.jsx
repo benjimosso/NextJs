47832c6df50c105024de308fe1535a8135b9c077
Daniel Mosso <dmosso@dhd.com>
2023-11-30 16:40:59 -0800

    fetch data 2

---
+++ helpdesk/app/tickets/TicketsList.jsx
@@ -0,0 +1,33 @@
+async function getTickets() {
+    const res = await fetch('http://localhost:4000/tickets', {
+        next: {
+            revalidate: 0 // use 0 to opt out of using cache, notice this may slower the application
+        }
+    })
+
+    return res.json()
+}
+
+
+
+export default async function TicketsList() {
+
+    const tickets = await getTickets()
+    return (
+      <>
+      {tickets.map((tickets) => (
+        <div key={tickets.id} className="card my-5">
+            <h3>{tickets.title}</h3>
+            <p>{tickets.body.slice(0, 200)}...</p>
+            <div className={`pill ${tickets.priority}`}>
+            {tickets.priority} priority
+            </div>
+            
+        </div>
+      ))}
+      {tickets.lenght === 0 && (
+        <p className="text-center"> There are no open tickets</p>
+      )}
+      </>
+    )
+}
--- helpdesk/app/tickets/page.jsx
+++ helpdesk/app/tickets/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import TicketsList from "./TicketsList"
 
 export default function Tickets() {
   return (
     <main>
-      <h2>Tickets</h2>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p><small>Currently open tickets</small></p>
+        </div>
+      </nav>
+      <TicketsList />
     </main>
   )
 }
