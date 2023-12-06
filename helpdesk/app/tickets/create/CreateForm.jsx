82d4c4692ba90dc4bc044c4d5c63b8cd4ed06ab9
Daniel Mosso <dmosso@dhd.com>
2023-12-05 16:47:54 -0800

    create form

--- helpdesk/app/tickets/create/CreateForm.jsx
+++ helpdesk/app/tickets/create/CreateForm.jsx
@@ -4,9 +4,33 @@ import { useRouter} from 'next/navigation'
 import { useState} from 'react'
 
 export default function CreateForm() {
+
+    const router = useRouter()
+
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [priority, setPriority] = useState('low')
+    const [isLoading, setIsLoading] = useState(false)
+
   return (
-    <div>
-      
-    </div>
+    <form className='w-1/2'>
+        <label >
+            <span>Title:</span>
+            <input 
+            required
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}/>
+        </label>
+        <label >
+        <span>Body:</span>
+            <input 
+            required
+            type='text'
+            onChange={(e) => setBody(e.target.value)}
+            value={body}/>
+        </label>
+        
+    </form>
   )
 }
