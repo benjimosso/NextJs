5ee6b63cda0a1fd0de620c722be6710db616cec5
Daniel Mosso <dmosso@dhd.com>
2023-12-05 16:44:36 -0800

    create form

---
+++ helpdesk/app/tickets/create/CreateForm.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useRouter} from 'next/navigation'
+import { useState} from 'react'
+
+export default function CreateForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ helpdesk/app/tickets/loading.jsx
@@ -0,0 +1,9 @@
+
+export default function Loading() {
+  return (
+    <main className="text-center">
+        <h2 className="text-primary">Loading...</h2>
+        <p>Hopefully not fot too long :)</p>
+    </main>
+  )
+}
